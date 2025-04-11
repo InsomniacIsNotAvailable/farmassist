cd51b39ab012dda87fc11b440ffa72b49abd605a
InsomniacIsNotAvailable <eorias.9146@umak.edu.ph>
2025-04-12 04:44:25 +0800

    Added About Us page

---
+++ resources/views/aboutus.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Signup</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+    <link rel="stylesheet" href="../../resources/css/aboutus.css">
+    <link rel="stylesheet" href="../../resources/css/global.css">
+    
+</head>
+<body>
+    <div class="header-spacer"></div>
+    <script src="http://localhost/farmassist/resources/js/header.js"></script>
+    <header class="header">
+        <img src="../../public/image/FARMASSIST (2000 x 500 px).svg" alt="FarmAssist Logo" class="logo">
+        <nav class="navbar">
+            <a href="#">MENU</a>
+            <a href="#">BRANCHES</a>
+            <a href="#">DELIVERY</a>
+            <a href="#">ABOUT US</a>
+            <a href="#" class="icon-container">
+                <i class="fa-solid fa-circle-user fa-2x"></i>
+            </a>
+            <a href="#"><i class="fa-solid fa-cart-shopping fa-2x"></i></a>
+        </nav>
+    </header>
+    <main>
+        <img class="aboutus-section" src="http://localhost/farmassist/public/image/ABOUTUS.png" alt="About us">
+        <div class="book-container">
+            <div class="left">
+                <div class="section-title">
+                    <span>WHO WE ARE</span>
+                </div>
+                <div class="text-container">
+                <h5>The Farmassist is a vegan restaurant based in Buting, Pasig City, founded in 2022.</h5>
+                <p>
+                    We believe healthy food shouldn't be boring or expensive. That's why we serve Healthy, Affordable, and Delicious meals that break the myth about vegan food. <br><br>
+                    At the same time, we're committed to supporting local farmers by giving value to their harvests and promoting fair pricing.<br><br>
+                    By choosing plant-based, we're also doing our part in protecting the planet from the harmful effects of livestock and poultry farming.<br><br>
+                    At The Farmassist, every meal is a step toward better health, stronger communities, and a greener Earth.
+                </p>
+                </div>
+                
+            </div>
+            <div class="right">
+                <img src="http://localhost/farmassist/public/image/aboutius.jpg" alt="Farmassist Image">
+            </div>
+        </div>
+        <div class="why-farmassist">
+            <div class="section-had">
+                <span>WHY THE FARMASSIST?</span>
+            </div>
+            <div class="why-content">
+                <div class="text">
+                    <h2><span class="highlight">H</span>EALTHY</h2>
+                    <h2><span class="highlight">A</span>FFORDABLE</h2>
+                    <h2><span class="highlight">D</span>ELICIOUS</h2>
+                </div>
+                <div class="image">
+                    <img src="http://localhost/farmassist/public/image/Vegetables.svg" alt="Fresh Vegetables">
+                </div>
+            </div>
+        </div>
+    </main>
+</body>
+</html>
